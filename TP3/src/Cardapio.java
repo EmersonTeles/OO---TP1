6057b2bc97a81caf62fc4b8832921ffecaa7a548
EmersonTeles <emersonteles21@gmail.com>
2021-09-10 01:26:34 -0300

    first commit

---
+++ TP3/src/Cardapio.java
@@ -0,0 +1,25 @@
+
+public class Cardapio {
+	private Produto produto;
+	private String categoria[];
+	public void cadastrar() {
+	}
+	public void editar() {	
+	}
+	public void deletar() {
+	}
+	public Produto getProduto() {
+		return produto;
+	}
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+	public String[] getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String[] categoria) {
+		this.categoria = categoria;
+	}
+	
+	
+}
